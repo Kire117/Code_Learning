ca50945e5b88239375468a3e5c4b34e9d8673884
Kire117 <126685770+Kire117@users.noreply.github.com>
2024-03-04 15:23:51 -0500

    New learnings! simple pygame game completed and start of a new course in Udemy!

---
+++ Python Total/Primer_Proyecto.py
@@ -0,0 +1,91 @@
+print("Hello World")
+
+print(100)
+
+print(100 + 50)
+
+# practica print 1
+# Crea un código que imprima en pantalla la expresión Me encanta estudiar Python.
+# ANSWER-------------------------------------------------------------------------
+print("Me encanta estudiar Python")
+
+# Practica print 2
+
+# Crea un código que imprima en pantalla la expresión Estudiar con "Python Total" es super divertido
+#ANSWER---------------------------------------------------------------------------------------------
+print('Estudiar con "Python Total" es super divertido')
+
+# Práctica Print 3
+# Crea un código que imprima en pantalla el número 555, pero no debes imprimirlo directamente,
+# sino como resultado de una operación matemática
+#ANSWER -----------------------------------------------------
+print(100+275+180)
+
+print ("Me llamo \"Kire\"")
+
+print("Esta es una linea\nY esta es otra linea")
+print("\tEsta es la tercera linea")
+
+
+# Práctica String 1
+# Crea un código que imprima en pantalla la siguiente expresión (pero usando print una sola vez):
+
+# Línea 1
+# Línea 2
+# Línea 3
+
+#ANSWER-----------------------
+print('Línea 1\nLínea 2\nLínea 3')
+
+# Practica string 2
+# Práctica String 2
+# Crea un código que imprima en pantalla la siguiente expresión.
+
+# A	B	C
+# D	E	F
+# G	H	I
+
+#ANSWER-----------------
+print('A\tB\tC\nD\tE\tF\nG\tH\tI')
+
+# Práctica String 3
+# Crea un código que imprima en pantalla la siguiente expresión:
+
+# Barra Normal: /
+# Barra Invertida: \
+
+# ANSWER-----------------
+print('Barra Normal: /\nBarra Invertida: \\')
+
+
+# INPUT
+
+print("Your full name is " + input("Write your name: ") + " " + (input("Write your last name: ")))
+
+# Práctica Input 1
+# Crea un código que le permita ingresar una respuesta al usuario, haciéndole la siguiente pregunta:
+
+# ¿Qué estás estudiando?
+
+# El código debe poder imprimir en pantalla lo ingresado por el usuario (utilizando print).
+
+print(input("¿Qué estás estudiando?"))
+
+# Práctica Input 2
+# Crea un código que le permita ingresar una respuesta al usuario, haciéndole la siguiente pregunta:
+
+# ¿En qué país vives?
+
+# El código debe poder imprimir en pantalla lo ingresado por el usuario (utilizando print).
+
+print(input("¿En qué país vives?"))
+
+# Práctica Input 3
+# Crea un código que muestre en pantalla el nombre completo del usuario, permitiéndole ingresar su nombre y apellido con las siguientes instrucciones:
+
+# Escribe tu nombre:
+# Escribe tu apellido:
+# El código debe poder imprimir en pantalla el nombre y apellido del usuario, separados por un espacio.
+
+print(input("Escribe tu nombre: ") + " " + (input("Escribe tu apellido: ")))
+
